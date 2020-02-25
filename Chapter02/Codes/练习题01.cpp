798754eaa70cdc586d451a9702038b4451cce9be
Soler索莱尔HO <25105794+SolerHo@users.noreply.github.com>
2020-02-25 20:55:29 +0800

    练习题01

---
+++ Chapter02/Codes/练习题01.cpp
@@ -0,0 +1,24 @@
+/********************************************************************
+ * 
+ * Date : 2020 - 02 - 25
+ * Author : Soler HO
+ * 
+ * Book : C++ Primer Plus 
+ * 
+ * description :  
+ * 编写一个程序，显示你的姓名和地址
+ * 
+ ********************************************************************/
+
+#include<iostream>
+
+using namespace std;
+
+int main(void)
+{
+    string name = "Soler HO";
+    string myAddress = "Shanghai";
+    cout<<"我的名字是："<<name<<","<<"我现在居住在"<<myAddress<<endl;
+
+    return 0;
+}
