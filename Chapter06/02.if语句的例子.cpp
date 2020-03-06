aea64c65125d7fa5c258e40143c15fd9cd11fca9
Soler索莱尔HO <25105794+SolerHo@users.noreply.github.com>
2020-03-06 16:01:34 +0800

    02 if语句的例子

---
+++ Chapter06/02.if语句的例子.cpp
@@ -0,0 +1,34 @@
+/********************************************************************
+ * 
+ * Date : 2020 - 03 - 06
+ * Author : Soler HO
+ * 
+ * Book : C++ Primer Plus
+ * 
+ * Chapter : FIVE
+ * 
+ * description :  if语句的例子
+ * 
+ * 
+ ********************************************************************/
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    char ch;
+    int spaces = 0;
+    int total = 0; // 统计字符数
+    cin.get(ch);
+
+    while(ch!='.') // 语句终止符号
+    {
+        if(ch == ' ') // 检查ch是否是空格
+            ++spaces;
+        ++total;
+        cin.get(ch);
+    }   
+    cout<<"一共是 "<<spaces<<" 个空格,"<<"一共是 "<<total<<" 个字符！\n";
+    return 0;
+}
