e87d4f1687a6dc4964538b1f90cec42b4f32ec89
Soler索莱尔HO <25105794+SolerHo@users.noreply.github.com>
2020-03-06 22:19:47 +0800

    14 switch语句中使用枚举常量作case标签

---
+++ Chapter06/14.switch语句中使用枚举常量作case标签.cpp
@@ -0,0 +1,58 @@
+/********************************************************************
+ * 
+ * Date : 2020 - 03 - 06
+ * Author : Soler HO
+ * 
+ * Book : C++ Primer Plus
+ * 
+ * Chapter : FIVE
+ * 
+ * description :  switch语句中使用枚举常量作case标签
+ * 
+ * 
+ ********************************************************************/
+#include<iostream>
+
+using namespace std;
+
+enum{red,orange,yellow,green,blue,violet,indigo};
+
+int main()
+{
+    cout<<"请输入颜色编号（0～6）：";
+    int code;
+    cin>>code;
+
+    while(code>=red && code<indigo)
+    {
+        switch(code)
+        {
+            case red:
+                    cout<<"her lips were red.\n";
+                    break;
+            case orange:
+                    cout<<"her hair was orange.\n";
+                    break;
+            case yellow:
+                    cout<<"her shoes were yellow.\n";
+                    break;
+            case green:
+                    cout<<"her nails were green.\n";
+                    break;
+            case blue:
+                    cout<<"her sweatsuit was blue.\n";
+                    break;
+            case violet:
+                    cout<<"her eyes were violet.\n";
+                    break;
+            case indigo:
+                    cout<<"her mood was indigo.\n";
+                    break;
+        }
+        cout<<"请输入颜色编号（0～6）：";
+        cin>>code;
+    }
+    cout<<"结束了！拜拜！"<<endl;
+
+    return 0;
+}
