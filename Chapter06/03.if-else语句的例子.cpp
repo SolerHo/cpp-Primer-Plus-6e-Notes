aa5de2e50d2f166c8c2e63104bc0700d114abbd4
Soler索莱尔HO <25105794+SolerHo@users.noreply.github.com>
2020-03-06 17:03:09 +0800

    03 if else语句的例子

---
+++ Chapter06/03.if-else语句的例子.cpp
@@ -0,0 +1,36 @@
+/********************************************************************
+ * 
+ * Date : 2020 - 03 - 06
+ * Author : Soler HO
+ * 
+ * Book : C++ Primer Plus
+ * 
+ * Chapter : FIVE
+ * 
+ * description :  if else语句的例子
+ * 
+ * 
+ ********************************************************************/
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    char ch;
+
+    cout<<"输入文本内容："<<endl;
+    cin.get(ch);
+
+    while(ch != '.')
+    {
+        if(ch == '\n')
+            cout<<ch;
+        else
+            cout<<++ch;
+        cin.get(ch);
+    }
+    cout<<"\n";
+
+    return 0;
+}
