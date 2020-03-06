f42c046288c470f4deaeb561f5d4c92d5b9b82a7
Soler索莱尔HO <25105794+SolerHo@users.noreply.github.com>
2020-03-06 18:26:11 +0800

    09 逻辑非（NOT）运算符

---
+++ Chapter06/09.逻辑NOT运算符.cpp
@@ -0,0 +1,45 @@
+/********************************************************************
+ * 
+ * Date : 2020 - 03 - 06
+ * Author : Soler HO
+ * 
+ * Book : C++ Primer Plus
+ * 
+ * Chapter : FIVE
+ * 
+ * description :  逻辑非（NOT）运算符：!
+ * 
+ * 
+ ********************************************************************/
+#include<iostream>
+#include<climits>
+
+using namespace std;
+
+bool is_int(double);
+
+int main()
+{
+    double num;
+
+    cout<<"Yoo!请输入一个整数：";
+    cin>>num;
+
+    while(!is_int(num)) // 不能超出int类型的取值范围
+    {
+        cout<<"超出范围了！-- 请重试：";
+        cin>>num;
+    }
+    int val = int(num); // 类型转换
+    cout<<"你已经输入一个整数："<<val<<"\nbye";
+
+    return 0;
+}
+
+bool is_int(double x)
+{
+    if(x<= INT_MAX && x>=INT_MIN) // 使用climits值
+        return true;
+    else
+        return false;
+}
