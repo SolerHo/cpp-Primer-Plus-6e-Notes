c6359fdf236bf42a44c00cbe91b7dcdc23d6ec26
Soler索莱尔HO <25105794+SolerHo@users.noreply.github.com>
2020-03-06 17:05:17 +0800

    06 逻辑OR运算符

---
+++ Chapter06/06.逻辑OR运算符.cpp
@@ -0,0 +1,38 @@
+/********************************************************************
+ * 
+ * Date : 2020 - 03 - 06
+ * Author : Soler HO
+ * 
+ * Book : C++ Primer Plus
+ * 
+ * Chapter : FIVE
+ * 
+ * description :  逻辑或（OR）运算符：||
+ * 
+ * 
+ ********************************************************************/
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"This program may reformat your hard disk\n"
+        "and destroy all your data\n"
+        "do you wish to continue?<Y/N>";
+
+    char ch;
+    cin>>ch;
+
+    if(ch == 'y' || ch == 'Y') /*大小写都需要判断，只需要读取一个字符即可*/
+        cout<<"You were warned!\a\a\n";
+    else if(ch == 'n'||ch == 'N')
+        cout<<"A wise choice...bye\n";
+    else
+    cout<<"That wasn't a y or n! apparently you "
+        "can't follow\ninstructions, so "
+        "I'll trash your disk anway.\a\a\n";
+
+    return 0;
+    
+}
