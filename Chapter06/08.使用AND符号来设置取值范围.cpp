3fee739bb2d835dc04664f6c259036d30a18cdfa
Soler索莱尔HO <25105794+SolerHo@users.noreply.github.com>
2020-03-06 17:32:21 +0800

    08 使用AND符号来设置取值范围

---
+++ Chapter06/08.使用AND符号来设置取值范围.cpp
@@ -0,0 +1,45 @@
+/********************************************************************
+ * 
+ * Date : 2020 - 03 - 06
+ * Author : Soler HO
+ * 
+ * Book : C++ Primer Plus
+ * 
+ * Chapter : FIVE
+ * 
+ * description :  使用&&符号来设置取值范围
+ * 
+ * 
+ ********************************************************************/
+#include<iostream>
+
+using namespace std;
+
+const char *qualify[4] = 
+{
+    "10000-meter race.\n",
+    "mud tug-of-war.\n",
+    "masters canoe jousting.\n",
+    "pie-throwing festivals.\n"
+};
+
+int main()
+{
+    int age;
+    cout<<"请输入你今年的年龄：";
+    cin>>age;
+    int index = 0;
+
+    if(age>17&&age<35)
+        index = 0;
+    else if(age>=35&&age<50)
+        index = 1;
+    else if(age>=50&&age<65)
+        index = 2;
+    else
+        index = 3;
+    
+    cout<<"你取得的资格是："<<qualify[index]<<endl;
+    
+    return 0;
+}
