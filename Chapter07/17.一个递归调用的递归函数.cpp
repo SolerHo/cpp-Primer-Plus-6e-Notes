8d00e81e66f29d044b9195b40b156f8224a5f723
Soler索莱尔HO <25105794+SolerHo@users.noreply.github.com>
2020-04-03 07:47:02 +0800

    Update 17.一个递归调用的递归函数.cpp

--- Chapter07/17.一个递归调用的递归函数.cpp
+++ Chapter07/17.一个递归调用的递归函数.cpp
@@ -5,8 +5,31 @@
  * 
  * Book : C++ Primer Plus 6 edition
  * 
- * Chapter : 
+ * Chapter : 07 
  * 
- * description : 
+ * description : 一个递归调用的递归函数
+ * 
+ * 什么是递归？
+ * 
+ * 自己调用自己的函数（C++中。main()函数不允许调用自己）叫做递归。
  * 
  ********************************************************************/
+#include<iostream>
+
+using namespace std;
+
+void countdown(int n);
+
+int main()
+{
+    countdown(5); // 调用递归函数
+    return 0;
+}
+
+void countdown(int n) // 每一个递归的调用都会创建一个变量，最终产生的的变量数是：n个。
+{
+    cout<<"倒计时......"<<n<<endl;
+    if(n>0)
+        countdown(n-1); // 自己调用自己的函数
+    cout<<n<<":Kaboom!\n";
+}
