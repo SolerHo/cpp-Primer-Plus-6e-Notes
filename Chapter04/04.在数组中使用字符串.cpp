e79ad936b9d95f03645a2445cce184655aec47c5
SolerHo <launolife@gmail.com>
2021-07-19 00:32:55 +0800

    更新chapter04学习笔记

--- Chapter04/04.在数组中使用字符串.cpp
+++ Chapter04/04.在数组中使用字符串.cpp
@@ -18,8 +18,8 @@
  ********************************************************************/
 
 
-#include<iostream>
-
+#include <iostream>
+#include <cstring>
 using namespace std;
 
 const int Size = 15;
@@ -28,25 +28,19 @@ int main()
 {
     char name1[Size];
     char name2[Size] = "C++owboy";
-
     // 字符串的拼接
     cout<<"Howdy!I'm "<< name2;
     cout<<"!,What's your name?\n";
     cin>>name1;
-
-
     // strlen()函数返回的是存储在数组中的字符串的长度，而不是数组本身的长度
     // strlen()只计算可见的字符，而不把空字符计算在内。
     cout<<"Well, "<<name1<<",your name has : "<<strlen(name1)<<" letters and is stored!\n" ;
     cout<<"In an array of "<<sizeof(name1)<<" Bytes\n";
-
     cout<<"Your iniatial is "<<name1[0]<<".\n"; // name1数组中的第一个元素
     name2[3] = '\0';
-
     cout<<"Here are the first 3 characters of my name:"<<name2<<endl;
 
     return 0;
-
 }
 
 
